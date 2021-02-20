9c855fa5458cfef4cde0881b9286fee3f27e7c9d
Lena Hou <lenahou2013@gmail.com>
2021-02-20 20:06:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,7 @@
 class Transfer
-<<<<<<< HEAD
+
   attr_reader :sender, :receiver, :amount
   attr_accessor :status
-=======
-  attr_accessor :sender, :receiver, :status, :amount
->>>>>>> 60221e94e5d0b95cb24fa46eb2457c83dc808bdc
 
   def initialize(sender, receiver, amount)
     @sender = sender
@@ -13,7 +10,7 @@ class Transfer
     @amount = amount
   end
 
-<<<<<<< HEAD
+
 def valid?
   @status == "pending" && @sender.valid? && @receiver.valid? && @sender.balance > @amount
 end
@@ -37,7 +34,4 @@ def reverse_transfer
 end
 end
 
-
-=======
->>>>>>> 60221e94e5d0b95cb24fa46eb2457c83dc808bdc
 end
