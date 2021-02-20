2f59e440e0d79f6d2d2ed3b849768ceaaeb4b1ac
Lena Hou <lenahou2013@gmail.com>
2021-02-20 20:04:56 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,11 @@
 class BankAccount
+<<<<<<< HEAD
 attr_accessor :balance, :status
 attr_reader :name
 
+=======
+attr_accessor :name, :balance, :status
+>>>>>>> 60221e94e5d0b95cb24fa46eb2457c83dc808bdc
 
 def initialize(name)
   @name = name
@@ -11,6 +15,7 @@ end
 
 def deposit(amount)
   @balance += amount
+<<<<<<< HEAD
 end
 
 def display_balance
@@ -24,5 +29,11 @@ end
 def close_account
 @status = "closed"
 end
+=======
+  @balance
+end
+
+
+>>>>>>> 60221e94e5d0b95cb24fa46eb2457c83dc808bdc
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,10 @@
 class Transfer
+<<<<<<< HEAD
   attr_reader :sender, :receiver, :amount
   attr_accessor :status
+=======
+  attr_accessor :sender, :receiver, :status, :amount
+>>>>>>> 60221e94e5d0b95cb24fa46eb2457c83dc808bdc
 
   def initialize(sender, receiver, amount)
     @sender = sender
@@ -9,6 +13,7 @@ class Transfer
     @amount = amount
   end
 
+<<<<<<< HEAD
 def valid?
   @status == "pending" && @sender.valid? && @receiver.valid? && @sender.balance > @amount
 end
@@ -33,4 +38,6 @@ end
 end
 
 
+=======
+>>>>>>> 60221e94e5d0b95cb24fa46eb2457c83dc808bdc
 end
